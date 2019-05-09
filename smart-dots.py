bb34189ade1bc224ba53c9b3b3cd0e7e4b1c7353
tobiabocchi <tobia97@gmail.com>
2019-05-09 12:02:15 +0200

    Changed colors for readability

--- smart-dots.py
+++ smart-dots.py
@@ -9,7 +9,7 @@ population = Population()
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, YELLOW)
+    text_surface = font.render(text, True, BLACK)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
